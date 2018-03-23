db3566b67732287672ee11e1a1fda704b3abf897
Héctor Astorga <hm.astorga@gmail.com>
2018-03-23 01:33:34 -0300

    fix: se imprimen correctamente los datos según enunciado

--- servidor_demo.py
+++ servidor_demo.py
@@ -2,7 +2,7 @@ import socket
 import os
 import json
 
-print("Servidor HTTP")
+print("Servidor HTTP\n")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -13,36 +13,31 @@ while True:
 
     client_connection, client_address = s.accept()
     request = client_connection.recv(1024)
-    #print(request)
     request_lines = request.split("\r\n")
-    #print(request_lines)
     request_line_0 = request_lines[0]
-    #print(request_line_0)
     items_line_0 = request_line_0.split(" ")
-    #print(items_line_0)
     request_path = items_line_0[1]
     filesystem_path = "./documentRoot/%s" % request_path
 
-    print(request_path)
-    print(filesystem_path)
+    # RUTAS
+    #print(request_path)
+    #print(filesystem_path)
 
     #diccionario
     headers = {}
     i=1
     while request_lines[i]:
 
-        primer_header = request_lines[i]
-        nombre_header, valor_header = primer_header.split(":", 1)
-        #print(nombre_header)
-        #print(valor_header)
+        lista_headers = request_lines[i]
+        nombre_header, valor_header = lista_headers.split(":", 1)
 
         headers[nombre_header] = valor_header
-        #print(headers)
 
         request_echo_header = {}
         request_echo_header["method"] = items_line_0[0]
         request_echo_header["headers"] = headers
         #print(request_echo_header)
+        #print(request_line_0)
         request_echo_json = json.dumps(request_echo_header)
         print(request_echo_json)
         i = i+1
@@ -54,12 +49,14 @@ while True:
 
         client_connection.sendall("HTTP/1.1 200 OK\r\n".encode())
         client_connection.sendall("X-RequestEcho: %s\r\n\r\n".encode() % request_echo_json)
-        client_connection.sendall("CONTENIDO: \n %s\r\n\r\n".encode() % archivo.read())
+        print(client_connection.send("HTTP/1.1 200 OK \r\n\r\n".encode()))
+        print(client_connection.send("X-RequestEcho: %s\r\n\r\n".encode() % request_echo_json))
+        client_connection.sendall("CONTENIDO: \n\r %s\n\r\n\r".encode() % archivo.read())
 
         archivo.close()
 
     else:
         print("404 archivo invalido")
-        client_connection.sendall("HTTP/1.1 404 Not Found".encode())
+        client_connection.sendall("HTTP/1.1 404 Not Found :'( ".encode())
 
     client_connection.close()
